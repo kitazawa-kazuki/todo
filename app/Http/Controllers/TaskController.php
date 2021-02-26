7ab0e71f64cc7666cae8a65c2c2447f70d33d1c2
DESKTOP-1KBS80H\k-kit <k-kitazawa@tri-ash.com>
2021-02-26 17:22:45 +0900

    Add:ToDoアプリのフォルダ作成機能を作成しました。

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,15 +7,21 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    public function index(int $id)
+    {
+        // すべてのフォルダを取得する
+        $folders = Folder::all();
 
-  public function index(int $id)
-  {
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
 
-    $folders = Folder::all();
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = $current_folder->tasks()->get();
 
-    return view('tasks/index', [
-      'folders' => $folders,
-      'current_folder_id' => $id,
-    ]);
-  }
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
 }
