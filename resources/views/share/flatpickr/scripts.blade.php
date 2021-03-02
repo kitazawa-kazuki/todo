c3c029e2fe46251e8ed9ced2a0aa39cf12f739f0
DESKTOP-1KBS80H\k-kit <k-kitazawa@tri-ash.com>
2021-03-02 15:02:02 +0900

    Add:ToDoアプリのタスク作成機能を作成しました。

---
+++ resources/views/share/flatpickr/scripts.blade.php
@@ -0,0 +1,9 @@
+<script src="https://npmcdn.com/flatpickr/dist/flatpickr.min.js"></script>
+<script src="https://npmcdn.com/flatpickr/dist/l10n/ja.js"></script>
+<script>
+  flatpicker(document,getElementById('due_date'), {
+    locale: 'ja',
+    dateformat: "Y/m/d",
+    minDate: new Date()
+  });
+</script>
