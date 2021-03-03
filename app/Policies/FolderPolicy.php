1e79817daac17208f5ca65ec1f120f70a6cf21a4
DESKTOP-1KBS80H\k-kit <k-kitazawa@tri-ash.com>
2021-03-03 18:11:33 +0900

    Add:エラーハンドリングを作成しました。

---
+++ app/Policies/FolderPolicy.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Folder;
+
+class FolderPolicy
+{
+
+
+  /**
+   * Create a new policy instance.
+   *
+   * @return void
+   */
+  public function view(user $user, Folder $folder)
+  {
+    return $user->id === $folder->user_id;
+  }
+}
