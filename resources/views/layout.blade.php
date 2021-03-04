8eb10c90b2af1d61945cce186add47bcc03b3eca
DESKTOP-1KBS80H\k-kit <k-kitazawa@tri-ash.com>
2021-03-04 12:24:21 +0900

    fix:headerにログイン、会員登録表示

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,9 +23,9 @@
           @csrf
         </form>
         @else
-        <a class="my-navbar-item" href="{{ route('login') }}" ログイン></a>
+        <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
         |
-        <a class='my-navbar-item' href="{{ route('register') }}" 会員登録></a>
+        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
         @endif
       </div>
     </nav>
